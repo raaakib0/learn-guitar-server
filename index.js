1d169bd32b004abe61a1ef1dda61f41628020086
md-rakibul-islam <raaakib0@gmail.com>
2022-11-14 00:47:39 +0600

    category data added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
     res.send('Learn guitar api running');
 });
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
+
 app.listen(port, () => {
-    console.log('server running', port);
+    console.log('learn guitar server running', port);
 })
