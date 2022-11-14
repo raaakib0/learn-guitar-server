8e3061733a559b6b072656b2b141e1d36c01bb18
md-rakibul-islam <raaakib0@gmail.com>
2022-11-15 00:57:26 +0600

    course data added and set up

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const categories = require('./data/categories.json');
+const course = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('Learn guitar api running');
@@ -13,6 +14,24 @@ app.get('/', (req, res) => {
 
 app.get('/course-categories', (req, res) => {
     res.send(categories)
+});
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '01') {
+        res.send(course);
+    }
+    else {
+        const category_course = course.filter(n => n.category_id === id);
+        res.send(category_course);
+    }
+    
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = course.find(n => n._id === id);
+    res.send(selectedCourse);
 })
 
 app.listen(port, () => {
